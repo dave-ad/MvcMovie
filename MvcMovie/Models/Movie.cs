211f8eb364d95204b52b8c42f1d3c9c6926dd01a
David <davidaderibigbe47@gmail.com>
2023-07-10 15:21:05 +0100

    +Validation

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -7,14 +7,26 @@ namespace MvcMovie.Models;
 public class Movie
 {
     public int Id { get; set; }
+
+    [StringLength(60, MinimumLength = 3)]
+    [Required]
     public string? Title { get; set; }
 
-    [Display(Name = "Release Date")]
-    [DataType(DataType.Date)]
+    [Display(Name = "Release Date"), DataType(DataType.Date)]
+    [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
     public DateTime ReleaseDate { get; set; }
-    public string? Genre { get; set; }
 
+    [Range(1, 100), DataType(DataType.Currency)]
     [Column(TypeName = "decimal(18, 2)")]
     public decimal Price { get; set; }
+
+    [RegularExpression(@"^[A-Z]+[a-zA-z\s]*$")]
+    [Required]
+    [StringLength(30)]
+    public string? Genre { get; set; }
+
+    [RegularExpression(@"^[A-Z]+[a-zA-z0-9""'\s-]*$")]
+    [StringLength(5)]
+    [Required]
     public string? Rating { get; set; }
 }
